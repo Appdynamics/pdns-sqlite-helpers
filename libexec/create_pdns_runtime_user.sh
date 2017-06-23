7066426c4f1207ca49581415afba0c040913ad09
Mike Przybylski <mike.przybylski@appdynamics.com>
2017-06-22 21:14:31 -0700

    Including RecordName attribute with initial dscl . -create for runtime user and group.

--- libexec/create_pdns_runtime_user.sh
+++ libexec/create_pdns_runtime_user.sh
@@ -14,10 +14,10 @@ source @SHAREDIR@/pdns-postgresql-helper-constants.sh
 if ! dscl . -list "/Users/$PDNS_RUNTIME_USER" >/dev/null 2>&1; then
     >&2 echo "Creating PowerDNS runtime user '$PDNS_RUNTIME_USER'"
     >&2 echo "sudo may prompt you for your password."
-    sudo dscl . -create /Users/$PDNS_RUNTIME_USER Password \*
+    sudo dscl . -create /Users/$PDNS_RUNTIME_USER RecordName $PDNS_RUNTIME_USER "$PDNS_USER_RECORD_NAME"
     sudo dscl . -create /Users/$PDNS_RUNTIME_USER UniqueID $PDNS_UNIQUE_ID
     sudo dscl . -create /Users/$PDNS_RUNTIME_USER UserShell /usr/bin/false
-    sudo dscl . -change /Users/$PDNS_RUNTIME_USER RecordName $PDNS_RUNTIME_USER "$PDNS_USER_RECORD_NAME"
+    sudo dscl . -create /Users/$PDNS_RUNTIME_USER Password \*
     sudo dscl . -create /Users/$PDNS_RUNTIME_USER RealName "$PDNS_REALNAME"
     sudo dscl . -create /Users/$PDNS_RUNTIME_USER PrimaryGroupID "$PDNS_PRIMARY_GID"
     sudo dscl . -delete /Users/$PDNS_RUNTIME_USER dsAttrTypeNative:accountPolicyData
@@ -27,7 +27,7 @@ fi
 if ! dscl . -list "/Groups/$PDNS_RUNTIME_GROUP" >/dev/null 2>&1; then
     >&2 echo "Creating PowerDNS runtime group '$PDNS_RUNTIME_USER'"
     >&2 echo "sudo may prompt you for your password."
+    sudo dscl . -create /Groups/$PDNS_RUNTIME_GROUP RecordName $PDNS_RUNTIME_GROUP "$PDNS_GROUP_RECORD_NAME"
     sudo dscl . -create /Groups/$PDNS_RUNTIME_GROUP PrimaryGroupID $PDNS_PRIMARY_GID
-    sudo dscl . -change /Groups/$PDNS_RUNTIME_GROUP RecordName $PDNS_RUNTIME_GROUP "$PDNS_GROUP_RECORD_NAME"
     sudo dscl . -create /Groups/$PDNS_RUNTIME_GROUP RealName "$PDNS_REALNAME"
 fi
